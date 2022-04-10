da8c1832da1c84cef4fa12c99b1d7f1e95efcf08
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2022-04-10 16:37:29 +0800

    Add project files.

---
+++ kdiff3-demo/kdiff3-demo.cpp
@@ -0,0 +1,12 @@
+﻿// kdiff3-demo.cpp : Defines the entry point for the application.
+//
+
+#include "kdiff3-demo.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
