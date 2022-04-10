9f9b65ee3d4f9e34aa7c64c5ede08045872899cf
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2022-04-10 16:51:42 +0800

    vector sort

--- kdiff3-demo/kdiff3-demo.cpp
+++ kdiff3-demo/kdiff3-demo.cpp
@@ -2,11 +2,20 @@
 //
 
 #include "kdiff3-demo.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+
+
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+  std::vector<int> v1{ 1,3,5,9,7 };
+  std::sort(v1.begin(), v1.end(), [](int l, int r) {
+    return l > r;
+    });
+  cout << "Hello CMake." << endl;
+  return 0;
 }
